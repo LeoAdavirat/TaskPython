047fe38d3f76ad2622a305502f2cb3f6fb6cfa78
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-11-10 14:02:25 +0700

    hi

--- PLCalc.py
+++ PLCalc.py
@@ -3,7 +3,8 @@
 from PropositionalLogic import *
 
 # calc = PropositionalLogic.Propositional_Logic()
-A = gen_table(2)
+# A = gen_table(2)
+A = gen_table(4)
 Ans = []
 def showit():
     for i in range(len(A)):
@@ -12,14 +13,13 @@ def showit():
 for i in range(len(A)):
     a=A[i][0]
     b=A[i][1]
+    c=A[i][2]
+    d=A[i][3]
+    # Ans.append(
+    #     ior(b, And(negate(d),c))
+    # )
     Ans.append(
-        ior(a,And(a, b))
-    )
-
-
-for i in range(len(A)):
-    Ans.append(
-        ior(A[0], And(A[0], A[1]))
+        And(And(eq(a,b), ior(negate(c),b)), And(negate(d),c))
     )
 
 showit()
