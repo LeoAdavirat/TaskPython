2273252e49d1c8cd51bf25c7f8af516b1d72a977
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2024-03-01 09:49:50 +0900

    AI

--- gBPL_Face_Recognition/index.py
+++ gBPL_Face_Recognition/index.py
@@ -1,112 +1,39 @@
-from typing import Tuple, Union
-import math
+i = 1022
 import cv2
-import numpy as np
-
-MARGIN = 10  # pixels
-ROW_SIZE = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-TEXT_COLOR = (255, 0, 0)  # red
-
-
-def _normalized_to_pixel_coordinates(
-    normalized_x: float, normalized_y: float, image_width: int, image_height: int
-) -> Union[None, Tuple[int, int]]:
-    """Converts normalized value pair to pixel coordinates."""
-
-    # Checks if the float value is between 0 and 1.
-    def is_valid_normalized_value(value: float) -> bool:
-        return (value > 0 or math.isclose(0, value)) and (
-            value < 1 or math.isclose(1, value)
-        )
-
-    if not (
-        is_valid_normalized_value(normalized_x)
-        and is_valid_normalized_value(normalized_y)
-    ):
-        # TODO: Draw coordinates even if it's outside of the image bounds.
-        return None
-    x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-    y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-    return x_px, y_px
-
-
-def visualize(image, detection_result) -> np.ndarray:
-    """Draws bounding boxes and keypoints on the input image and return it.
-    Args:
-      image: The input RGB image.
-      detection_result: The list of all "Detection" entities to be visualize.
-    Returns:
-      Image with bounding boxes.
-    """
-    annotated_image = image.copy()
-    height, width, _ = image.shape
-
-    for detection in detection_result.detections:
-        # Draw bounding_box
-        bbox = detection.bounding_box
-        start_point = bbox.origin_x, bbox.origin_y
-        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-        cv2.rectangle(annotated_image, start_point, end_point, TEXT_COLOR, 3)
-
-        # Draw keypoints
-        for keypoint in detection.keypoints:
-            keypoint_px = _normalized_to_pixel_coordinates(
-                keypoint.x, keypoint.y, width, height
-            )
-            color, thickness, radius = (0, 255, 0), 2, 2
-            cv2.circle(annotated_image, keypoint_px, thickness, color, radius)
-
-        # Draw label and score
-        category = detection.categories[0]
-        category_name = category.category_name
-        category_name = "" if category_name is None else category_name
-        probability = round(category.score, 2)
-        result_text = category_name + " (" + str(probability) + ")"
-        text_location = (MARGIN + bbox.origin_x, MARGIN + ROW_SIZE + bbox.origin_y)
-        cv2.putText(
-            annotated_image,
-            result_text,
-            text_location,
-            cv2.FONT_HERSHEY_PLAIN,
-            FONT_SIZE,
-            TEXT_COLOR,
-            FONT_THICKNESS,
-        )
-
-    return annotated_image
-
-
-# !wget -q -O image.jpg https://i.imgur.com/Vu2Nqwb.jpg
-
-IMAGE_FILE = "D:\Code\TaskPython\gBPL_Face_Recognition\image.jpg"
-
-import cv2
-
-# from google.colab.patches import cv2_imshow
-
-img = cv2.imread(IMAGE_FILE)
-cv2.imshow(img)
-# STEP 1: Import the necessary modules.
-import numpy as np
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 
-# STEP 2: Create an FaceDetector object.
-base_options = python.BaseOptions(model_asset_path="detector.tflite")
-options = vision.FaceDetectorOptions(base_options=base_options)
-detector = vision.FaceDetector.create_from_options(options)
+# import dlib
+from PIL import Image
+from skimage import io
 
-# STEP 3: Load the input image.
-image = mp.Image.create_from_file(IMAGE_FILE)
+mp_face_detection = mp.solutions.face_detection.FaceDetection(
+    min_detection_confidence=0.7
+)
+mp_drawing = mp.solutions.drawing_utils
 
-# STEP 4: Detect faces in the input image.
-detection_result = detector.detect(image)
+webcam = cv2.VideoCapture(0)
+while webcam.isOpened():
+    success, img = webcam.read()
+    i += 1
+    # face detection using MediaPipe
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = mp_face_detection.process(img)
 
-# STEP 5: Process the detection result. In this case, visualize it.
-image_copy = np.copy(image.numpy_view())
-annotated_image = visualize(image_copy, detection_result)
-rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-cv2.imshow(rgb_annotated_image)
+    # draw the face detection annotations on the image
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    if results.detections:
+        for detection in results.detections:
+            mp_drawing.draw_detection(img, detection)
+            a = str(detection).split()
+            xmin = int(float(a[11]) * 640)
+            ymin = int(float(a[13]) * 480)
+            width = int(float(a[15]) * 640)
+            height = int(float(a[17]) * 480)
+            cropped_img = img[ymin : ymin + height, xmin : xmin + height]
+            cv2.imwrite("dataset/image{}.jpg".format(i), cropped_img)
+    cv2.imshow("Koolac", img)
+    if cv2.waitKey(5) & 0xFF == ord("q"):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
---
+++ gBPL_Face_Recognition/sketch_feb29b/sketch_feb29b.ino
@@ -0,0 +1,178 @@
+//LATEST CODE
+
+#include <LiquidCrystal.h>
+#include <Stepper.h>
+
+LiquidCrystal lcd(12, 7, 6, 5, 4, 3);  // Initialize the LCD library with the pins you are using
+
+const int buttonHour = A0; // Pin for the button to adjust hours
+const int buttonMinute = A1; // Pin for the button to adjust minutes
+const int buttonSecond = A2; // Pin for the button to adjust seconds
+const int buttonReset = A3; // Pin for the button to reset the timer
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+long int second = 4000;
+
+int initialHours = 0; // Store initial countdown hours
+int initialMinutes = 0; // Store initial countdown minutes
+int initialSeconds = 0; // Store initial countdown seconds
+int previousMillis = 0;
+
+bool startPressed = false; // Flag to track if the start button has been pressed
+bool timerFinished = false; // Flag to indicate if the timer has finished
+
+// Pin definitions
+const int buttonPin = 2;
+const int ledPin = 13;
+const int motorPins[] = {8, 9, 10, 11};
+
+// Define the number of steps per revolution
+const int stepsPerRevolution = 200;
+
+// Initialize the stepper motor object with the number of steps and the pins it's connected to
+Stepper myStepper(stepsPerRevolution, motorPins[0], motorPins[1], motorPins[2], motorPins[3]);
+//void displayTime();
+//void runStepperMotor();
+// Variables to track button state and timing
+bool allowButtonPress = true;
+long int buttonMillis = 0;
+bool blinkingLED = false; // Changed to false initially
+unsigned long lastButtonPressTime = 0;
+
+bool motorRan = false; // Flag to track if the motor has run
+
+
+
+void runStepperMotor() {
+  
+  // Rotate the motor for 1/3 of a full rotation
+  myStepper.step(stepsPerRevolution / 3);
+
+  // Optional delay after motor movement
+  delay(500); // Adjust as needed
+  // Rotate the motor for 1/3 of a full rotation
+  myStepper.step(stepsPerRevolution / 3);
+
+  // Optional delay after motor movement
+  delay(500); // Adjust as needed
+  // Rotate the motor for 1/3 of a full rotation
+  myStepper.step(stepsPerRevolution / 3);
+
+  // Optional delay after motor movement
+  delay(500); // Adjust as needed
+  // Rotate the motor for 1/3 of a full rotation
+  myStepper.step(stepsPerRevolution / 3);
+
+  // Optional delay after motor movement
+  delay(500); // Adjust as needed
+}
+
+void displayTime() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  int tempSecond = second/1000;
+  int displayHour = tempSecond / 3600;
+  int displayMinute = (tempSecond - displayHour*60)/60;
+  int displaySecond = tempSecond - displayHour * 3600 - displayMinute * 60;
+  lcd.print("Time: " + String(displayHour) + ":" + String(displayMinute) +  ":" + String(displaySecond));
+  Serial.println("Time: " + String(displayHour) + ":" + String(displayMinute) +  ":" + String(displaySecond));
+  // Serial.println(second);
+  // ONLY DISPLAY IF IT'S MORE THAN 0
+}
+
+
+void setup() {
+  lcd.begin(16, 2);  // Set up the LCD's number of columns and rows
+  pinMode(buttonHour, INPUT_PULLUP); // Set button pins as input with pullup resistors
+  pinMode(buttonMinute, INPUT_PULLUP);
+  pinMode(buttonSecond, INPUT_PULLUP);
+  pinMode(buttonReset, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT); // Initialize LED pin
+  digitalWrite(ledPin, LOW); // Turn off LED initially
+  myStepper.setSpeed(60); // Set the speed of the stepper motor
+  
+  displayTime();
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Display on LCD
+  displayTime();
+  // Serial.println(millis());
+  Serial.println(millis() - buttonMillis);
+  // Check hour button
+  if (digitalRead(buttonHour) == LOW) {
+    if (millis() - buttonMillis >= 3000) {
+      allowButtonPress = true;
+    }
+    if (allowButtonPress) {second += 3600000;}
+  }
+  // Check minute button
+  if (digitalRead(buttonMinute) == LOW) {
+    if (millis() - buttonMillis >= 3000) {
+      allowButtonPress = true;
+    }
+    if (allowButtonPress) {second += 60000;}
+  }
+
+  // check second button
+  if (digitalRead(buttonSecond) == LOW) {
+    if (millis() - buttonMillis >= 3000) {
+      allowButtonPress = true;
+    }
+    if (allowButtonPress) {second++;}
+  }
+
+  // check reset button
+  if (digitalRead(buttonReset) == LOW) {
+    if (millis() - buttonMillis >= 3000) {
+      allowButtonPress = true;
+    }
+    if (allowButtonPress) {
+      hours = initialHours;
+      minutes = initialMinutes;
+      seconds = initialSeconds;
+
+      // CONFIGURE LATER
+      // motorRan = false; // Reset the motor run flag
+      // digitalWrite(ledPin, LOW); // Turn off LED
+      // blinkingLED = false; // Disable blinking
+      // // Start button pressed for the first time
+      // startPressed = true;
+    }
+  }
+
+  int interval = millis() - previousMillis;
+  second -= interval;
+
+  // Blink LED logic
+  if (blinkingLED) {
+    digitalWrite(ledPin, !digitalRead(ledPin)); // Toggle LED state
+    delay(500);
+  }
+  // Check if the button is pressed
+  if (digitalRead(buttonPin) == LOW) {
+    if (second <= 0)
+    {// Stop LED blinking
+    blinkingLED = false;
+    digitalWrite(ledPin, 0);
+    // Run stepper motor for 1/3 of a full rotation
+    runStepperMotor();
+
+    motorRan = true; // Set motor run flag}
+  }
+
+  // Check if the motor has run
+  if (motorRan) {
+    // Reset countdown timer to initial time
+    hours = initialHours;
+    minutes = initialMinutes;
+    seconds = initialSeconds;
+    displayTime();
+    motorRan = false; // Reset motor run flag
+  }
+  previousMillis = millis();
+}
+}
